89ec7265949e62dc87b941559a1e9186cbeafddd
Invra <identificationsucks@gmail.com>
2025-10-05 03:25:03 +1100

    Add (nix): overlay This commit changes the flake to follow a few tadards of formatting but main actual additions are: * New package output, default * An overlay which people can use on pkgs to simply specify `pkgs.csharp-language-server`.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   description = "A flake for csharp-language-server";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        csharp-language-server = pkgs.rustPlatform.buildRustPackage {
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    let
+      overlay = _: super: {
+        csharp-language-server = super.rustPlatform.buildRustPackage {
           checkFlags = [
             # Test is unable to persist files while testing in nix
             "--skip=first_line_is_jsonrpc"
@@ -19,20 +24,30 @@
 
           src = ./.;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
+          cargoLock.lockFile = ./Cargo.lock;
 
-          nativeBuildInputs = [ pkgs.pkgs.dotnetCorePackages.dotnet_8.sdk ];
-
+          nativeBuildInputs = [ super.dotnetCorePackages.dotnet_8.sdk ];
+        };
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
         };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [ csharp-language-server ];
+          buildInputs = with pkgs; [ csharp-language-server ];
         };
 
-        packages.csharp-language-server = csharp-language-server;
+        packages = with pkgs; {
+          default = csharp-language-server;
+          inherit csharp-language-server;
+        };
       }
-    );
+    ) // {
+      overlays.default = overlay;
+    };
 }
